b45bfa7977d293b11bd86ab0b954e384501c3b60
HanseongBae <bha4388@naver.com>
2022-12-04 20:13:40 +0900

    mento-service

---
+++ mztalk-mento-service/src/test/java/com/mztalk/mento/MztalkMentoServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mztalk.mento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MztalkMentoServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
