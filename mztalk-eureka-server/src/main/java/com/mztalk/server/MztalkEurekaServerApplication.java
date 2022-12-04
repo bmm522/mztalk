901133cdd34ee5263370c72c4a6278ac2c3a178a
bmm522 <rlawldls11@naver.com>
2022-12-04 19:45:34 +0900

    final start

---
+++ mztalk-eureka-server/src/main/java/com/mztalk/server/MztalkEurekaServerApplication.java
@@ -0,0 +1,13 @@
+package com.mztalk.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MztalkEurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MztalkEurekaServerApplication.class, args);
+	}
+
+}
---
+++ mztalk-eureka-server/src/test/java/com/mztalk/server/MztalkEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mztalk.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MztalkEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
