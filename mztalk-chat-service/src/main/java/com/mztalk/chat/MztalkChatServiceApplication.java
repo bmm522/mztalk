23d685cc9aabdebbad7a848dc848974db90d5d9a
nvr1195 <nvr1195@gmail.com>
2022-12-05 17:38:14 +0900

    create chat service

---
+++ mztalk-chat-service/src/main/java/com/mztalk/chat/MztalkChatServiceApplication.java
@@ -0,0 +1,13 @@
+package com.mztalk.chat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MztalkChatServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MztalkChatServiceApplication.class, args);
+	}
+
+}
