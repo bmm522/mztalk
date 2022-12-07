dd1ecd47db53465f615a4f27fe9813d07da5c1a3
nvr1195 <nvr1195@gmail.com>
2022-12-07 11:37:28 +0900

    chat

---
+++ mztalk-chat-service/src/main/java/com/mztalk/chat/controller/TestController.java
@@ -0,0 +1,16 @@
+package com.mztalk.chat.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/chat")
+@RestController
+public class TestController {
+
+    @GetMapping("/test")
+    public String Test() {
+
+        return "test";
+    }
+}
