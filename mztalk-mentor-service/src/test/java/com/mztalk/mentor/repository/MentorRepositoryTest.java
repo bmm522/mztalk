51c2406234b93a57ea3626c8fee907f1e6382888
HanseongBae <bha4388@naver.com>
2022-12-08 16:30:22 +0900

    feat : test Mentor Entiry, Mentor Repository

--- mztalk-mentor-service/src/test/java/com/mztalk/mentor/repository/MentorRepositoryTest.java
+++ mztalk-mentor-service/src/test/java/com/mztalk/mentor/repository/MentorRepositoryTest.java
@@ -1,22 +1,18 @@
 package com.mztalk.mentor.repository;
 
+import com.mztalk.mentor.domain.Entity.Mentor;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
+import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.*;
 
 @SpringBootTest
 @Transactional
 class MentorRepositoryTest {
-
-    @PersistenceContext
-    private EntityManager em;
 
     @Autowired
     private MentorRepository mentorRepository;
@@ -24,11 +20,52 @@ class MentorRepositoryTest {
     @Test
     public void saveTest() throws Exception {
         //given
+        Mentor mentor1 = new Mentor();
+        Mentor mentor2 = new Mentor();
 
         //when
+        Mentor savedMentor1 = mentorRepository.save(mentor1);
+        Mentor savedMentor2 = mentorRepository.save(mentor2);
 
         //then
-
+        assertThat(mentor1).isEqualTo(savedMentor1);
+        assertThat(mentor2).isEqualTo(savedMentor2);
     }
+
+    @Test
+    public void findTest() throws Exception {
+        //given
+        Mentor mentor1 = new Mentor();
+        Mentor mentor2 = new Mentor();
+
+        mentorRepository.save(mentor1);
+        mentorRepository.save(mentor2);
+
+        //when
+        Mentor findMentor1 = mentorRepository.findById(mentor1.getId()).get();
+        Mentor findMentor2 = mentorRepository.findById(mentor2.getId()).get();
+
+        //then
+        assertThat(mentor1).isEqualTo(findMentor1);
+        assertThat(mentor2).isEqualTo(findMentor2);
+    }
+
+    @Test
+    public void findAllTest() throws Exception {
+        //given
+        Mentor mentor1 = new Mentor();
+        Mentor mentor2 = new Mentor();
+        mentorRepository.save(mentor1);
+        mentorRepository.save(mentor2);
+
+        //when
+        List<Mentor> mentorList = mentorRepository.findAll();
+
+        //then
+        assertThat(mentorList.size()).isEqualTo(2);
+        assertThat(mentorList.get(0)).isEqualTo(mentor1);
+        assertThat(mentorList.get(1)).isEqualTo(mentor2);
+    }
+
 
 }
