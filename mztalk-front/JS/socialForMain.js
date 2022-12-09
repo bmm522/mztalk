681caf2a7d4f5517c6243e50dc738fe1f22bc00f
bmm522 <rlawldls11@naver.com>
2022-12-09 16:14:08 +0900

    feat : 메인페이지 백업

---
+++ mztalk-front/JS/socialForMain.js
@@ -0,0 +1,6 @@
+window.addEventListener('DOMContentLoaded', function(){
+ console.log('window addEventListener DOMContentLoaded');
+ var httpRequest = new XMLHttpRequest();
+ console.log(httpRequest.getAllResponseHeaders());
+});
+ 
