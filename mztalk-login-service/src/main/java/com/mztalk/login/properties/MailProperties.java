6f30ad93218d6a687fc6a0095f415c2e7d2d091e
bmm522 <102157839+bmm522@users.noreply.github.com>
2022-12-11 21:32:42 +0900

    Delete MailProperties.java

--- mztalk-login-service/src/main/java/com/mztalk/login/properties/MailProperties.java
+++
@@ -1,8 +0,0 @@
-package com.mztalk.login.properties;
-
-public interface MailProperties {
-    String HOST ="smtp.naver.com";
-    String PORT = "465";
-    String MAIL_ID = "rlawldls11@naver.com";
-    String MAIL_PW = "dlwngns";
-}
