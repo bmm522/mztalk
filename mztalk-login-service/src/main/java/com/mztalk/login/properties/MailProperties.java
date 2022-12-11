946cb9870988ec9f6af8c973a0184ae8261a845a
bmm522 <rlawldls11@naver.com>
2022-12-11 22:22:43 +0900

    fix : 백업

---
+++ mztalk-login-service/src/main/java/com/mztalk/login/properties/MailProperties.java
@@ -0,0 +1,8 @@
+package com.mztalk.login.properties;
+
+public interface MailProperties {
+    String HOST ="smtp.naver.com";
+    String PORT = "465";
+    String MAIL_ID = "rlawldls11@naver.com";
+    String MAIL_PW = "dlwngns";
+}
