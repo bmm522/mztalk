934f7b35562ca343c86950eb6c961b9967e26c3c
bmm522 <102157839+bmm522@users.noreply.github.com>
2022-12-11 21:34:22 +0900

    Delete JwtProperties.java

--- mztalk-login-service/src/main/java/com/mztalk/login/properties/JwtProperties.java
+++
@@ -1,8 +0,0 @@
-package com.mztalk.login.properties;
-
-public interface JwtProperties {
-    String SECRET = "secret1234";
-    int EXPIRATION_TIME=600000;
-    String TOKEN_PREFIX = "Bearer ";
-    String HEADER_STRING ="Authorization";
-}
