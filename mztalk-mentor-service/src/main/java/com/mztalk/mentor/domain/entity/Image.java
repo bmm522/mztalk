0b7a26664e7b41f0e6081c159af63a8546d76335
HanseongBae <bha4388@naver.com>
2022-12-16 12:29:12 +0900

    refactor : Image Entity Protected설정

--- mztalk-mentor-service/src/main/java/com/mztalk/mentor/domain/entity/Image.java
+++ mztalk-mentor-service/src/main/java/com/mztalk/mentor/domain/entity/Image.java
@@ -11,7 +11,7 @@ import java.util.UUID;
 
 @Entity
 @Getter
-@NoArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Table(name="IMAGE")
 public class Image extends com.mztalk.mentor.domain.entity.BaseTimeEntity {
 
