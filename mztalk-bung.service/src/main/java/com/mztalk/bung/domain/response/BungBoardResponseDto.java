bd9eea136f2eb193cb84079312e71e92740f9ecc
bmm522 <rlawldls11@naver.com>
2022-12-19 18:05:01 +0900

    feat : bung 프론트 포스트, 리드 완료

--- mztalk-bung.service/src/main/java/com/mztalk/bung/domain/response/BungBoardResponseDto.java
+++ mztalk-bung.service/src/main/java/com/mztalk/bung/domain/response/BungBoardResponseDto.java
@@ -1,5 +1,17 @@
 package com.mztalk.bung.domain.response;
 
+import com.mztalk.bung.domain.entity.BungBoard;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class BungBoardResponseDto {
 
     private String imageUrl;
@@ -17,4 +29,18 @@ public class BungBoardResponseDto {
     private String deadlineDate;
 
 
+    public BungBoardResponseDto(BungBoard bungBoard, String imageUrl, String imageName) {
+        this.imageUrl = imageUrl;
+        this.imageName = imageName;
+        this.count = String.valueOf(bungBoard.getBoardCount());
+        this.title = bungBoard.getBoardTitle();
+        this.content = bungBoard.getBoardContent();
+        this.category = bungBoard.getCategory();
+        this.writer = bungBoard.getBoardWriter();
+        this.nowGroup = bungBoard.getNowGroup();
+        this.fullGroup = bungBoard.getFullGroup();
+        this.deadlineDate = String.valueOf(bungBoard.getDeadlineDate());
+
+
+    }
 }
--- mztalk-bung.service/src/main/java/com/mztalk/bung/service/impl/BungServiceImpl.java
+++ mztalk-bung.service/src/main/java/com/mztalk/bung/service/impl/BungServiceImpl.java
@@ -4,16 +4,24 @@ import com.mztalk.bung.domain.BoardStatus;
 import com.mztalk.bung.domain.dto.BungBoardDto;
 import com.mztalk.bung.domain.entity.BungBoard;
 import com.mztalk.bung.domain.entity.Result;
+import com.mztalk.bung.domain.response.BungBoardResponseDto;
 import com.mztalk.bung.exception.BoardException;
 import com.mztalk.bung.repository.BungBoardRepository;
 import com.mztalk.bung.repository.BungBoardRepositoryCustom;
 import com.mztalk.bung.service.BungBoardService;
 import lombok.RequiredArgsConstructor;
+import org.json.JSONObject;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import javax.transaction.Transactional;
 import java.sql.Date;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.stream.Collectors;
@@ -55,8 +63,33 @@ public class BungServiceImpl implements BungBoardService {
     @Override
     public Result mainSelectList() {
         List<BungBoard> bungBoards = bungRepository.findAll();
-        List<BungBoardDto> collect = bungBoards.stream().map(BungBoardDto::new).collect(Collectors.toList());
-        return new Result(collect);
+        List<BungBoardResponseDto> bungBoardResponseDtoList = new ArrayList<>();
+        for(BungBoard bungBoard : bungBoards){
+            HttpHeaders headers = new HttpHeaders();
+            headers.add("Content-Type", "text/html");
+            System.out.println("service : " + bungBoard.getBoardTitle());
+            ResponseEntity<String> response = new RestTemplate().exchange(
+                    "http://localhost:8000/resource/main-image?bNo="+bungBoard.getBoardId()+"&serviceName=bung",
+                    HttpMethod.GET,
+                    new HttpEntity<String>(headers),
+                    String.class
+            );
+            JSONObject jsonObject = new JSONObject(response.getBody());
+            JSONObject jsonData = jsonObject.getJSONObject("data");
+            String imageUrl = jsonData.getString("imageUrl");
+            String imageName = jsonData.getString("objectKey");
+
+        //   bungBoardResponseDtoList = bungBoards.stream().map(l -> new BungBoardResponseDto(bungBoard, imageUrl, imageName)).collect(Collectors.toList());
+
+//            System.out.println(bungBoardResponseDtoList.get(1));
+        bungBoardResponseDtoList.add(new BungBoardResponseDto(bungBoard, imageUrl, imageName));
+
+        }
+//        System.out.println(bungBoardResponseDtoList.get(0).getTitle());
+//        System.out.println(bungBoardResponseDtoList.get(1).getTitle());
+//        List<BungBoardResponseDto> list = bungBoards.stream().map(l ->  bungBoardResponseDtoList).collect(Collectors.toList());
+//        List<BungBoardDto> collect = bungBoards.stream().map(BungBoardDto::new).collect(Collectors.toList());
+        return new Result(bungBoardResponseDtoList);
     }
 
     // 메인 게시글 수정
