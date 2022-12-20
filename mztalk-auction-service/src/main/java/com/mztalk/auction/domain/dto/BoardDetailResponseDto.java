65fff622c0d84991800eef1e80f6260aaf574f68
bmm522 <rlawldls11@naver.com>
2022-12-20 22:40:24 +0900

    refactor : auction detail refactor

--- mztalk-auction-service/src/main/java/com/mztalk/auction/domain/dto/BoardDetailResponseDto.java
+++ mztalk-auction-service/src/main/java/com/mztalk/auction/domain/dto/BoardDetailResponseDto.java
@@ -1,5 +1,6 @@
 package com.mztalk.auction.domain.dto;
 
+import com.mztalk.auction.domain.entity.Board;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -18,4 +19,12 @@ public class BoardDetailResponseDto {
     private String content;
     private String currentPrice;
     private List<ConcurrentHashMap<String, String>> imageInfo;
+
+    public BoardDetailResponseDto(Board board, List<ConcurrentHashMap<String, String>> imageInfo) {
+        this.boardId = board.getBoardId();
+        this.title = board.getTitle();
+        this.content = board.getContent();
+        this.currentPrice = String.valueOf(board.getCurrentPrice());
+        this.imageInfo = imageInfo;
+    }
 }
