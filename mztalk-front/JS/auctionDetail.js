dc0ec00744ec24dc06e0c18806cc1d191f03aedc
갸갸 <quietwave00@gmail.com>
2022-12-20 14:48:25 +0900

    chore: front 수정

--- mztalk-front/JS/auctionDetail.js
+++ mztalk-front/JS/auctionDetail.js
@@ -6,7 +6,7 @@ let currentIdx = 0;
 const slideCount = slideImg.length; //슬라이드 개수
 const prev = document.querySelector('.prev');
 const next = document.querySelector('.next');
-const slideWidth = 300;
+const slideWidth = 900;
 const slideMargin = 100;
 
 slides.style.width = (slideWidth + slideMargin) * slideCount + 'px';
