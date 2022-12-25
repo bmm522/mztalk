d5df7808d36e1b559a8e8150a662e38a843731f8
bmm522 <rlawldls11@naver.com>
2022-12-25 14:19:01 +0900

    feat : 관리자 승인 함수 구현 완료

--- mztalk-front/admin/js/admin.js
+++ mztalk-front/admin/js/admin.js
@@ -15,6 +15,7 @@ window.onload = function(){
                 let email = application.email;
                 let createDate = application.createdDate.substr(0,9);
                 let authStatus = application.authStatus;
+                let userNo = application.mentor.userId;
             if(authStatus.includes('NO')){              
                 document.getElementById('table-body').innerHTML += `<tr>
                 <td>${name}</td>
@@ -23,7 +24,7 @@ window.onload = function(){
                 <td>${email}</td>
                 <td>${createDate}</td>
                 <td>${authStatus}</td>
-                <td><button style="cursor:pointer;"  type="button" id="reg-btn">신청받기</button></td>
+                <td><div id="${userNo}"><button style="cursor:pointer;"  type="button" id="reg-btn" onclick="admit('${userNo}')">신청받기</button></div></td>
             </tr>`;
             } else{
                 document.getElementById('table-body').innerHTML += `<tr>
@@ -38,4 +39,25 @@ window.onload = function(){
             }
         }
     })
+}
+
+const admit = (userNo) =>{
+
+    fetch('http://localhost:8000/mentors/member', {
+        method:"POST",
+        headers:{
+            "Content-Type":"application/json",
+            Authorization:localStorage.getItem('authorization'),
+            RefreshToken:localStorage.getItem('refreshToken'),
+        },
+        body:JSON.stringify({
+            "userId" : userNo
+        })
+    })
+    .then(res=>{
+        alert('신청 받기 완료');
+        document.getElementById(userNo).remove();
+        location.href="index.html";
+    })
+    
 }
