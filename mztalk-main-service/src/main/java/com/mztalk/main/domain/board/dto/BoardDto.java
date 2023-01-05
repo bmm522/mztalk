7f0d8c6c64aa56877fcf5bf59756069534459100
Ssantoo <nowing0108@kakao.com>
2023-01-05 12:13:40 +0900

    feat: 스웨거 ui 추가

--- mztalk-main-service/src/main/java/com/mztalk/main/domain/board/dto/BoardDto.java
+++ mztalk-main-service/src/main/java/com/mztalk/main/domain/board/dto/BoardDto.java
@@ -6,6 +6,7 @@ import com.mztalk.main.domain.profile.dto.ProfileResponseDto;
 import com.mztalk.main.domain.reply.Reply;
 import com.mztalk.main.status.BoardStatus;
 import com.mztalk.main.status.PrivacyStatus;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -20,25 +21,34 @@ import java.util.List;
 @NoArgsConstructor
 @AllArgsConstructor
 public class BoardDto {
-
+    @ApiModelProperty(value="글 번호", example = "1", required = true)
     private Long id; //글번호
 
-    //private Board writer; //작성자
+    @ApiModelProperty(value="글 제목", example = "글 제목", required = true)
     private String title; //글제목
+    @ApiModelProperty(value="글 내용", example = "오늘 하루 행복", required = true)
     private String content; //글내용
+    @ApiModelProperty(value="글 상태", example = "PUBLIC", required = true)
     private BoardStatus status; //글상태
+    @ApiModelProperty(value="페이지 주인", example = "1", required = true)
     private Long own; //페이지주인
+
+    @ApiModelProperty(value="글 공개범위", example = "PUBLIC", required = true)
     private PrivacyStatus privacy; //글공개범위
-
+    @ApiModelProperty(value="글생성 날짜", example = "2023-01-01'T'12:00:00", required = true)
     private LocalDateTime createdDate;
-
+    @ApiModelProperty(value="글수정 날짜", example = "2023-01-01'T'12:00:00", required = true)
     private LocalDateTime lastModifiedDate;
 
+    @ApiModelProperty(value="서비스 이름", example = "STORY", required = true)
     private String serviceName;
+
+    @ApiModelProperty(value="댓글 리스트/대댓글 가능", example = "반가워", required = true)
     private List<Reply> replyList = new ArrayList<>();
 
+    @ApiModelProperty(value="own의 프로필 이미지 URL", example = "https://mztalk-resource-server.amazonaws.com/프로필이미지.png", required = true)
     private String postImageUrl;
-
+    @ApiModelProperty(value="글작성자", example = "남나눔", required = true)
     private String writer;
 
     //레포지토리에 넣기위해
