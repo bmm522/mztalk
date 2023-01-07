b4c4b73974f56c66e006319c8139785d389dcbda
Ssantoo <nowing0108@kakao.com>
2023-01-08 04:26:57 +0900

    feat 팔로우 스웨거 작성

--- mztalk-main-service/src/main/java/com/mztalk/main/config/SwaggerConfig.java
+++ mztalk-main-service/src/main/java/com/mztalk/main/config/SwaggerConfig.java
@@ -3,32 +3,30 @@ package com.mztalk.main.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import springfox.documentation.builders.ApiInfoBuilder;
-import springfox.documentation.builders.ParameterBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
-import springfox.documentation.service.Parameter;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
-
-import java.util.ArrayList;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
 
+
+
+@EnableWebMvc
 @Configuration
 @EnableSwagger2
-public class SwaggerConfig {
+public class SwaggerConfig implements WebMvcConfigurer {
 
 
     @Bean
     public Docket swagger(){
-
-        ParameterBuilder parameterBuilder = new ParameterBuilder();
-        List<Parameter> globalParamters = new ArrayList<>();
 
         return new Docket(DocumentationType.SWAGGER_2)
                 .ignoredParameterTypes(java.sql.Date.class)
@@ -36,7 +34,7 @@ public class SwaggerConfig {
                 .produces(getProduceContentTypes())
                 .forCodeGeneration(true)
                 .select()
-                .apis(RequestHandlerSelectors.basePackage("com.mztalk.main.controller"))
+                .apis(RequestHandlerSelectors.basePackage("com.mztalk.story.controller"))
                 .paths(PathSelectors.any())
                 .build()
                 .apiInfo(apiInfo())
@@ -48,7 +46,6 @@ public class SwaggerConfig {
                 .title("MZTALK STORY SERVICE API")
                 .description("STORY SERVICE 소개")
                 .contact(new Contact("Ssanto", "Ssanto.tistory.com", "nowing0108@gmail.com"))
-                .version("1.0")
                 .build();
     }
 
@@ -67,5 +64,23 @@ public class SwaggerConfig {
         produces.add("multipart/form-data");
         return produces;
     }
+
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("swagger-ui.html")
+                .addResourceLocations("classpath:/META-INF/resources/");
+
+        registry.addResourceHandler("/webjars/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/");
+        // -- Static resources
+        registry.addResourceHandler("/static/**").addResourceLocations("classpath:/static/");
+        registry.addResourceHandler("/css/**").addResourceLocations("classpath:/static/css");
+        registry.addResourceHandler("/js/**").addResourceLocations("classpath:/static/js/");
+        registry.addResourceHandler("/images/**").addResourceLocations("classpath:/static/images/");
+    }
+
+
+
 
 }
