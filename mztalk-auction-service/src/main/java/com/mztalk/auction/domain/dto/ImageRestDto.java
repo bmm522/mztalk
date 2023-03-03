cee067407cb0af8fc3c90517a3c99b3758cbb101
갸갸 <quietwave00@gmail.com>
2023-03-03 19:17:06 +0900

    refactor: 게시물 조회 dto 메소드 수정

--- mztalk-auction-service/src/main/java/com/mztalk/auction/domain/dto/ImageRestDto.java
+++ mztalk-auction-service/src/main/java/com/mztalk/auction/domain/dto/ImageRestDto.java
@@ -1,20 +1,26 @@
 package com.mztalk.auction.domain.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import org.json.JSONArray;
 import org.json.JSONObject;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 
+import java.awt.*;
+import java.util.ArrayList;
 import java.util.List;
 
+@AllArgsConstructor
+@NoArgsConstructor
 @Getter
 public class ImageRestDto {
 
     private String imageUrl;
     private String objectKey;
-    private List<ImageRestDto> imageRestDtoList;
 
-    public ImageRestDto(){}
     public ImageRestDto(ResponseEntity<String> response){
         JSONObject jsonObject = new JSONObject(response.getBody());
         JSONObject jsonData = jsonObject.getJSONObject("data");
@@ -23,13 +29,16 @@ public class ImageRestDto {
         this.objectKey = jsonData.getString("objectKey");
     }
 
+
     public List<ImageRestDto> getImageRestDtoList(ResponseEntity<String> response) {
         JSONObject jsonObject = new JSONObject(response.getBody());
         JSONArray data = jsonObject.getJSONArray("data");
+        List<ImageRestDto> imageRestDtoList = new ArrayList<>();
         for(int i = 0; i < data.length(); i++) {
-            ImageRestDto imageRestDto = new ImageRestDto(response);
-            this.imageRestDtoList.add(imageRestDto);
+            JSONObject resultObject = data.getJSONObject(i);
+            ImageRestDto imageRestDto = new ImageRestDto(resultObject.getString("imageUrl"), resultObject.getString("objectKey"));
+            imageRestDtoList.add(imageRestDto);
         }
-        return this.imageRestDtoList;
+        return imageRestDtoList;
     }
 }
